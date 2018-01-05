ff1ac650cdd331358bc1e34977d62cd9176aba6f
Shahzoda Davlatova <shahzoda01@gmail.com>
2018-01-05 16:48:39 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	
+	<head>
+    <?php include("header.html");?>
+	</head>
+	
+	<body>
+		<div class="intro-wrapper">
+			<div class="intro-banner">
+				<h1>WiCS Mentorship Initiative.</h1>
+				<div>
+					<p>Women in Computer Science Mentorship Initiative at Brooklyn college strives to build 
+						 a community of women in technology related fields and empower women in tech by providing
+						 leadership roles.</p>
+            <div class="section socials">
+                <i class="fa fa-2x fa-facebook-official" aria-hidden="true"></i>
+                <i class="fa fa-2x fa-linkedin-square" aria-hidden="true"></i>
+                <i class="fa fa-2x fa-envelope" aria-hidden="true"></i>
+            </div>
+        </div>
+				<div class="intro-buttons">
+            <button type="button" class="btn btn-primary" data-toggle="tooltip" data-placement="bottom" title="Learn more about us!" onclick="location.href='learn-more'">
+                Learn More
+            </button>
+            <button type="button" class="btn btn-primary" data-toggle="tooltip" data-placement="bottom" title="Join our mailing list!" onclick="location.href='https://wicsmentorprogram.typeform.com/to/yVKn7e'">
+                Sign Up
+            </button>
+            <button type="button" class="btn btn-primary" data-toggle="tooltip" data-placement="bottom" title="Volunteer or Mentor others!" onclick="location.href='https://wicsmentorprogram.typeform.com/to/zjYP96'">
+                Get Involved
+            </button>
+            <br class="clear" />
+				</div>
+			</div>
+		</div>
+	</body>
+	
+</html>
---
+++ learn-more.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+    <?php include("header.html");?>
+	</head>
+    
+	<body>
+    <div class="learn-more-wrapper">
+        <div class="learn-more-banner">
+            <h1>WiCS Mentorship Initiative.</h1>
+        </div>
+    </div>
+      
+    <div class="contents container">
+        <div class="section">
+            <h2>About</h2>
+            <p>The drop off rate for women studing computer science or a related feild is an incredibly high. The female to male 
+                ratio in the tech industry is unsatisfactory. With even the biggest company still making women in tech a rarity. Diversity 
+                is best for soceity, children, and companies themselves. We believe through a community of supportive women, we can make a 
+                difference. Our model has three pillars: empowerment through leadership roles, community of women of all ages, and insight
+                into the field, as well as insight into the next generation. The community helps keep women in tech. Leadership 
+                roles are offered to remind women they can do anything they want and help build their resumes with numerous skills. 
+                Lastly, we hope that students who want to learn more about the tech-related industry get a chance to speak to women
+                and learn from their experiences.</p>
+        </div>
+        <div class="section">
+            <h2>History</h2>
+            <p>Women in Computer Science Mentorship Initiative, or WiCSMI, was founded in 2016 from the Women In Computer Science 
+                Club on campus. The first Director was Chava Shulman, and she was incredibly instrumental to getting the initiative
+                up and running. Chava worked closely with the Magner Career Center's Director, Natalia, to lay the foundation of the 
+                initiative. Since then, there has been two Co-Directors running the program and it has been ever evolving based on 
+                feedback we recieve.</p>
+        </div>
+        <div class="section">
+            <h2>Co-Director</h2>
+            <hr/>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="profile-pics">
+                            <a>
+                                <img/ src="assets/temp.jpg">
+                            </a>
+                        </div>
+                        <div class="profile-info">
+                            <p class="name">Chava Shulman</p>
+                            <p class="major">Computer Science</p>
+                            <p class="years">Fall'16-Spring'18</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="profile-pics">
+                            <a>
+                                <img src=assets/Shahzoda.jpg>
+                            </a>
+                        </div>
+                        <div class="profile-info">
+                            <p class="name">Shahzoda Davlatova</p>
+                            <p class="major">Computer Engineering</p>
+                            <p class="years">Fall'17-Spring'19</p>
+                        </div>
+                    </div>
+                </div> 
+            </div>
+        </div>
+        <div class="section learn-more-buttons">
+            <h2>Join us!</h2>
+            <hr/>
+            <div class="container">
+            <button type="button" class="btn btn-primary" data-toggle="tooltip" data-placement="bottom" title="Join our mailing list!" onclick="location.href='sign-up'">
+                Sign Up
+            </button>
+            <button type="button" class="btn btn-primary" data-toggle="tooltip" data-placement="bottom" title="Volunteer or Mentor others!" onclick="location.href='get-inolved'">
+                Get Involved
+            </button>
+            <button type="button" class="btn btn-primary" data-toggle="tooltip" data-placement="bottom" title="Go back to the main page!" onclick="location.href='index'">
+                Main Page
+            </button>
+            </div>
+        </div>
+    </div>
+	</body>
+</html>
